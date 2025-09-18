76fb1a33393437184c3aa87a5193ecd3d84374bd
Sivakrishna Atkuri <sivakrishnaatkuri@gmail.com>
2025-09-18 11:02:00 +0000

    Use black color and increase the font for the carrierguide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
       <div className="flex flex-col min-h-screen bg-background text-foreground">
         <header className="absolute top-0 left-0 right-0 p-4 z-20">
              <div className="container mx-auto px-4 flex justify-between items-center">
-                <h2 className="text-2xl font-bold text-primary tracking-widest uppercase">CarrierGuide</h2>
+                <h2 className="text-3xl font-bold text-foreground tracking-widest uppercase">CarrierGuide</h2>
                 <Button asChild variant="link" className="text-primary text-lg">
                     <Link href="/login">
                         Log In
