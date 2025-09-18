dc08a98950b563285f772f765d1c81cd11f1ccf6
Sai kumar <saikumard912@gmail.com>
2025-09-18 20:53:07 +0000

    https://www.swarnprastha.com/wp-content/uploads/2020/06/2-Career-Guidanc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,9 +73,9 @@ export default function Home() {
                   </Button>
                 </div>
                 <div className="relative flex justify-center items-center animate-fade-in-up" style={{ perspective: '1000px', animationDelay: '0.2s' }}>
-                     <div className="bg-secondary/50 rounded-lg p-4 border transition-transform duration-500 ease-in-out hover:-rotate-y-12 hover:rotate-x-12 hover:scale-105 hover:shadow-2xl" style={{ transformStyle: 'preserve-3d' }}>
+                     <div className="bg-secondary/50 rounded-lg p-4 border transition-transform duration-500 ease-in-out hover:-translate-y-12 hover:rotate-x-12 hover:scale-105 hover:shadow-2xl" style={{ transformStyle: 'preserve-3d' }}>
                       <Image
-                        src="https://tse3.mm.bing.net/th/id/OIP.izOD3O6tVqgrhAyy5cXWewHaE8?pid=Api&P=0&h=220"
+                        src="https://www.swarnprastha.com/wp-content/uploads/2020/06/2-Career-Guidance-12-4.jpg"
                         alt="A woman holding a laptop and thinking about career guidelines."
                         width={600}
                         height={400}
